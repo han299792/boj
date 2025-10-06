c6f4065ad3c807444345c109144bb7102a368b96
hansemin <han299792@gmail.com>
2025-10-06 21:19:03 +0900

    1006

--- 2000/2156.py
+++ 2000/2156.py
@@ -1,7 +1,21 @@
 import sys
-
 data = sys.stdin.read().split()
 
 N = int(data[0])
-T = list(map(int, data[1:]))
+T = [0] + list(map(int, data[1:]))  
 
+dp = [0] * (N + 1)
+
+if N >= 1:
+    dp[1] = T[1]
+if N >= 2:
+    dp[2] = T[1] + T[2]
+
+for i in range(3, N + 1):
+    dp[i] = max(
+        dp[i-1],          
+        dp[i-2] + T[i],    
+        dp[i-3] + T[i-1] + T[i] 
+    )
+
+print(dp[N])
