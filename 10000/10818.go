0b4937ae6387ef6f88f0094645134fbc2791f340
hansemin <han299792@gmail.com>
2025-10-23 15:52:45 +0900

    이거어렵다

---
+++ 10000/10818.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n, num int
+	var min, max int
+
+	fmt.Fscan(reader, &n)
+
+	fmt.Fscan(reader, &num)
+	min = num
+	max = num
+
+	for i := 1; i < n; i++ {
+		fmt.Fscan(reader, &num)
+
+		if num > max {
+			max = num
+		}
+		if num < min {
+			min = num
+		}
+	}
+
+	fmt.Fprint(writer, min, " ", max)
+}
