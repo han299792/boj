8f5383c45342cc008f70d26698133e4081230280
hansemin <han299792@gmail.com>
2025-11-12 21:33:53 +0900

    1112

---
+++ 9000/9251.py
@@ -0,0 +1,15 @@
+import sys
+data = sys.stdin.read().split()
+A, B = data[0], data[1]
+a = len(A)
+b = len(B)
+dp = [[0]*(b+ 1) for _ in range(a+1) ]
+
+for i in range(1, a+1):
+    for j in range(1, b+1):
+        if A[i-1] == B[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[a][b])
